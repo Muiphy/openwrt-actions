d7f3f0d23168f60b1fb36548eb0aed14ee3d8896
Muiphy <githubit@163.com>
2020-12-13 02:59:15 -0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,3 +2,11 @@
 
 echo "Apply custom.sh"
 
+echo "clone luci-theme-argon"
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+echo "clone openclash"
+git clone https://github.com/vernesong/OpenClash.git
+pwd
+ls
+mv OpenClash/luci-app-openclash openwrt/package/
